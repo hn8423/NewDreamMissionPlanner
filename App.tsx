2e559700d28141a2957e196c11d7d74652195c04
hn8423 <rolenche@gmail.com>
2022-12-27 17:06:00 +0900

    user-agent 변경

--- App.tsx
+++ App.tsx
@@ -10,6 +10,7 @@
 
 import React, {type PropsWithChildren} from 'react'
 import {
+  Platform,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -45,9 +46,14 @@ const App = () => {
     <SafeAreaView style={[styles.wrapper]}>
       <WebView
         source={{uri: 'https://newdream.link'}}
-        userAgent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+        // userAgent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
         sharedCookiesEnabled={true}
         thirdPartyCookiesEnabled={true}
+        userAgent={
+          Platform.OS === 'android'
+            ? 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Mobile Safari/537.36'
+            : 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1'
+        }
       />
     </SafeAreaView>
   )
